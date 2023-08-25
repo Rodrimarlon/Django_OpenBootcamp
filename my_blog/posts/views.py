8021f8ae3971b383aba300fba3829f93c9e9e732
rodrimarlon <marlonrodriguezmartinez@gmail.com>
2023-08-24 22:45:12 -0400

    Creado consultas, en la aplicacion posts

--- my_blog/posts/views.py
+++ my_blog/posts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Author, Entry
 
-# Create your views here.
+
+def queries(request):
+    #Obtener todos los elementos
+    authors = Author.objects.all() 
+
+    # Obtener datos filtrados por condición
+    filtered = Author.objects.filter(email='wlove@example.net')
+
+    # Obtener un unico objeto (filtrado)
+    author = Author.objects.get(id=10)
+
+    # Obtener solo 10 elementos
+    limits = Author.objects.all()[40::-1]
+
+    return render(request, 'post/queries.html', 
+                  {'authors': authors, 'filtered': filtered,
+                   'author': author, 'limits': limits})
