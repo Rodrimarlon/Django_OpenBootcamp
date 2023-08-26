476763e4c8c011e4be31c45368e9438d40ae303f
rodrimarlon <marlonrodriguezmartinez@gmail.com>
2023-08-26 16:14:00 -0400

    Terminado de ver las consultas y la actualizacion de datos en el proyecto my_blog

--- my_blog/posts/views.py
+++ my_blog/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Author, Entry
+from django.http import HttpResponse
 
 
 def queries(request):
@@ -7,7 +8,7 @@ def queries(request):
     authors = Author.objects.all() 
 
     # Obtener datos filtrados por condición
-    filtered = Author.objects.filter(email='wlove@example.net')
+    #filtered = Author.objects.filter(email='wlove@example.net')
 
     # Obtener un unico objeto (filtrado)
     author = Author.objects.get(id=10)
@@ -15,6 +16,36 @@ def queries(request):
     # Obtener solo 10 elementos
     limits = Author.objects.all()[40::-1]
 
+    # Obtener todos los elementos ordenados
+    orders = Author.objects.all().order_by('email')
+
+    # Obtener todos los elementos cuyo id sea menor o igual a 15
+    """ Los para metros para pasarcelos a filter y realizar estas consultas son:
+        __lte : menor o igual que (lower than equals)
+        __gte : mayor o igual que (greater than equals)
+        __lt : menor que (lower than)
+        __gt : mayor que (greater than)
+        __contains : contiene
+        __exact : exacto
+    """
+    # filtered = Author.objects.filter(id__lte=15)
+
+    #Obtener todos los autores que contienen en su nombre la palabra yes
+    filtered = Author.objects.filter(name__contains="the")
+
+
+
+
     return render(request, 'post/queries.html', 
                   {'authors': authors, 'filtered': filtered,
-                   'author': author, 'limits': limits})
+                   'author': author, 'limits': limits,
+                   'orders': orders})
+
+
+def update(request):
+    author = Author.objects.get(id=1)
+    author.name = 'Marlon'
+    author.email = 'marlon@example.com'
+    author.save()
+
+    return HttpResponse('Datos Actualizados')
