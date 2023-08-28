684ff4ef94b7b97a0772ce9c1add5a437d83c9df
rodrimarlon <marlonrodriguezmartinez@gmail.com>
2023-08-27 21:54:29 -0400

    Creando tablas con relaciones uno a muchos y muchos a unos, para realizar las consultas requeridas

--- relations/many_to_one/models.py
+++ relations/many_to_one/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+            return self.email
+
+
+class Article(models.Model):
+      headline = models.CharField(max_length=100)
+      date = models.DateField()
+      reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+      def __str__(self):
+          return self.headline
+      
+    
+
--- relations/many_to_one/views.py
+++ relations/many_to_one/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Reporter, Article
+from datetime import date
+
 
 def create(request):
-    return HttpResponse('creado')
+    rep = Reporter(first_name='Marlon', last_name='Rodriguez', email='marlon@example.com')
+    rep.save()
+
+    art1 = Article(headline='Articulo 1', date=date(2022,5,5), reporter=rep)
+    art1.save()
+
+    art2 = Article(headline='Articulo 2', date=date(2023,4,8), reporter=rep)
+    art2.save()
+
+    art3 = Article(headline='Articulo 3', date=date(2020,5,7), reporter=rep)
+    art3.save()
+
+    # Consultando el email del reportero que creo el articulo 1
+    #result = art1.reporter.email
+
+    # Atrabes de _set vamos a acceder a los elmentos, y de hay hacer
+    # nuestras Consultas
+    result = rep.article_set.all()
+
+
+    return HttpResponse(result)
