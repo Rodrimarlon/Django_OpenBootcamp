d6470067f718495da7527462b9815f42ba47afe3
rodrimarlon <marlonrodriguezmartinez@gmail.com>
2023-10-10 21:34:51 -0400

    iniciando con la creacion de los estilos

--- Agenda_Pesonal_Practica/Agenda_Pesonal_Practica/urls.py
+++ Agenda_Pesonal_Practica/Agenda_Pesonal_Practica/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', include('contacts.urls')),
     path('to_do', include('to_do.urls')),
+    path('', views.index, name='index')
 ]
